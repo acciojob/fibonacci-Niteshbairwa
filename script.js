b4f6f033293f8b3cfb9c867f84b64dae0bd71517
Nitesh Bairwa <61491323+Niteshbairwa@users.noreply.github.com>
2024-09-18 11:00:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function fibonacci(num) {
-// your code here
+	let n1 =0; 
+	let n2 =1;
+	let nextterm;
+for(let i=0; i<=num; i++){
+	nextterm = n1+n2;
+	n1= n2;
+	n2 = nextterm;
+	return: n1;
+	
+	
+}
 }
 
 module.exports = fibonacci;
